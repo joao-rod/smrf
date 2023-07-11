058fa10f5cefb3dfb8f7cf0737a933cabdac7e16
Joao Victor <jvr2564@gmail.com>
2023-07-11 17:25:26 -0300

    feat: create project

---
+++ smrf/src/main/java/br/edu/ifnmg/smrf/SmrfApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifnmg.smrf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmrfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmrfApplication.class, args);
+	}
+
+}
---
+++ smrf/src/test/java/br/edu/ifnmg/smrf/SmrfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifnmg.smrf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmrfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
