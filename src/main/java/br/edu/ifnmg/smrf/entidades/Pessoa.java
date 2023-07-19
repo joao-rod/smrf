54ee65f592b3ee362a98291d2783c3e0e744afa2
yasmin <yasminribeiro@gmail.com>
2023-07-19 16:17:06 -0300

    feat: entidade pessoa

---
+++ src/main/java/br/edu/ifnmg/smrf/entidades/Pessoa.java
@@ -0,0 +1,102 @@
+package br.edu.ifnmg.smrf.entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="pessoas")
+public class Pessoa {
+    @Id
+    private long id;
+
+    @Column(length=100)
+    private String nome;
+
+    @Column(length=11)
+    private String cpf;
+
+    @Column
+    private String rg;
+
+    @Column
+    private String endereco;
+
+    public Pessoa() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pessoa other = (Pessoa) obj;
+        if (id != other.id)
+            return false;
+        if (cpf == null) {
+            if (other.cpf != null)
+                return false;
+        } else if (!cpf.equals(other.cpf))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa [nome=" + nome + "]";
+    }
+    
+}
